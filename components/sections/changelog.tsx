5a508175dafc6f9d82b272b32c0e3adb62b27384
shaan-m0 <shaanalam369@gmail.com>
2025-09-07 17:15:34 +0530

    added animations to changelogs section

--- components/sections/changelog.tsx
+++ components/sections/changelog.tsx
@@ -1,6 +1,7 @@
 import { lifeChangelog } from "@/data";
 import { motion } from "motion/react";
 import Image from "next/image";
+import { InView } from "../ui/in-view";
 
 const Changelog = () => {
   return (
@@ -15,30 +16,51 @@ const Changelog = () => {
       </motion.h1>
       <div className="mt-6">
         {lifeChangelog.map((changelog) => (
-          <div>
-            <h2 className="text-2xl font-semibold">{changelog.year}</h2>
-            <div>
-              {changelog.events.map((event) => (
-                <div key={event.title} className="my-4 px-3 py-4 border-b">
-                  <h3 className="text-lg font-semibold flex gap-x-4">
-                    <span>
-                      <Image
-                        src="/verified.svg"
-                        alt=""
-                        width={30}
-                        height={30}
-                        className="rounded-full"
-                      />
-                    </span>
-                    {event.title}
-                  </h3>
-                  <p className="ml-12 text-muted-foreground text-base">
-                    {event.description}
-                  </p>
-                </div>
-              ))}
+          <InView
+            once
+            key={changelog.year}
+            variants={{
+              hidden: {
+                opacity: 0,
+                y: 30,
+                scale: 0.95,
+                filter: "blur(4px)",
+              },
+              visible: {
+                opacity: 1,
+                y: 0,
+                scale: 1,
+                filter: "blur(0px)",
+              },
+            }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            viewOptions={{ margin: "0px 0px -200px 0px" }}
+          >
+            <div key={changelog.year}>
+              <h2 className="text-2xl font-semibold">{changelog.year}</h2>
+              <div>
+                {changelog.events.map((event) => (
+                  <div className="my-4 px-3 py-4 border-b">
+                    <h3 className="text-lg font-semibold flex gap-x-4">
+                      <span>
+                        <Image
+                          src="/verified.svg"
+                          alt=""
+                          width={30}
+                          height={30}
+                          className="rounded-full"
+                        />
+                      </span>
+                      {event.title}
+                    </h3>
+                    <p className="ml-12 text-muted-foreground text-base">
+                      {event.description}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
+          </InView>
         ))}
       </div>
     </div>
