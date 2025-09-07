cfc88a917ad1c43613bfb52862658ec933840bf1
shaan-m0 <shaanalam369@gmail.com>
2025-09-07 17:23:16 +0530

    added hover animations to projects section

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -3,8 +3,11 @@ import { projects } from "@/data";
 import { motion } from "motion/react";
 import Link from "next/link";
 import { AnimatedGroup } from "../ui/animated-group";
+import { useState } from "react";
 
 const Projects = () => {
+  const [hovered, setHovered] = useState<number | null>(null);
+
   return (
     <div className="my-12 w-[40%] mx-auto">
       <motion.h1
@@ -25,7 +28,7 @@ const Projects = () => {
         code snippets, blogs on technologies or in general what I think about
         programming here.
       </motion.p>
-      <div className="posts">
+      <div className="posts" onMouseLeave={() => setHovered(null)}>
         <AnimatedGroup
           className="flex gap-4 h-fit"
           variants={{
@@ -56,41 +59,50 @@ const Projects = () => {
             },
           }}
         >
-          {projects.map((project) => (
-            <div
-              key={project.title}
-              className="grayscale w-full hover:grayscale-0 relative hover:shadow-2xl border border-border rounded-xl p-4 overflow-hidden [&>div]:hover:grayscale-0 transition-all duration-500"
-            >
-              <div className="relative">
-                <img
-                  src={project.icon}
-                  width={50}
-                  height={50}
-                  alt={project.title}
+          {projects.map((project, idx) => (
+            <div>
+              {hovered === idx && (
+                <motion.div
+                  layoutId="hovered"
+                  className="absolute inset-0 -z-10 h-full w-full p-4 rounded-md bg-gray-100 dark:bg-neutral-900"
                 />
-                <h1 className="text-primary text-xl font-semibold my-3">
-                  {project.title}
-                </h1>
-                <p className="text-muted-foreground text-sm w-3/4">
-                  {project.description}
-                </p>
-                <div className="relative z-10 flex items-center space-x-3 mt-4">
-                  <a
-                    href={project.live_link}
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-muted-foreground text-sm underline-effect"
-                  >
-                    Live Demo
-                  </a>
-                  <Link
-                    href={project.github_repo_link}
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-muted-foreground text-sm underline-effect"
-                  >
-                    Github Repo
-                  </Link>
+              )}
+              <div
+                onMouseEnter={() => setHovered(idx)}
+                key={project.title}
+                className="grayscale w-full hover:grayscale-0 relative hover:shadow-2xl border border-border rounded-md p-4 overflow-hidden [&>div]:hover:grayscale-0 transition-all duration-500"
+              >
+                <div className="relative">
+                  <img
+                    src={project.icon}
+                    width={50}
+                    height={50}
+                    alt={project.title}
+                  />
+                  <h1 className="text-primary text-xl font-semibold my-3">
+                    {project.title}
+                  </h1>
+                  <p className="text-muted-foreground text-sm w-3/4">
+                    {project.description}
+                  </p>
+                  <div className="relative z-10 flex items-center space-x-3 mt-4">
+                    <a
+                      href={project.live_link}
+                      rel="noreferrer"
+                      target="_blank"
+                      className="text-muted-foreground text-sm underline-effect"
+                    >
+                      Live Demo
+                    </a>
+                    <Link
+                      href={project.github_repo_link}
+                      rel="noreferrer"
+                      target="_blank"
+                      className="text-muted-foreground text-sm underline-effect"
+                    >
+                      Github Repo
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
