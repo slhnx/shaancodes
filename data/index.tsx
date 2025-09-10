8a3b7cd4890c3e06b05c51e36f4157193a729af6
shaan-m0 <shaanalam369@gmail.com>
2025-09-10 05:42:13 +0530

    changed cover image of a featured post

--- data/index.tsx
+++ data/index.tsx
@@ -31,7 +31,7 @@ export const featuredPosts = [
     lastModified: new Date("09/14/2023"),
     author: "Shaan Alam",
     coverImage:
-      "https://y4cpsre1ob.ufs.sh/f/4HrWTRZRvKfT2XLBgSeNA4eBoGmOETcs7tZ30YlVKDhnx6Sf",
+      "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fovzuhgjr872cnslume6a.gif",
     tags: [],
     category: "Projects",
     featured: true,
