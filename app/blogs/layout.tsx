0daeb42a763dc2a3a4343fce40cb442224ec3e89
shaanalam <shaanalam369@gmail.com>
2025-12-14 19:36:36 +0530

    feat: styled article page

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -3,11 +3,9 @@ import { PropsWithChildren } from "react";
 const BlogLayout = async ({ children }: PropsWithChildren) => {
   return (
     <div className="blog-layout w-full  mx-auto text-secondary-foreground relative">
-      {children}
-      <div className="bg-white absolute bottom-0 left-0 -translate-y-1/2 h-[100px] w-full bg-opacity-15 blur-[80px] z-10"></div>
+      <article className="w-[50%] mx-auto my-36 article">{children}</article>
     </div>
   );
 };
 
 export default BlogLayout;
-
