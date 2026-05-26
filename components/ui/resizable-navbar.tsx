5d274ff945c677f0eb1464add12286bfbdaa8767
slhnx <shaanalam369@gmail.com>
2026-05-26 23:57:01 +0530

    refactor: remove outdated comment regarding navbar positioning in resizable-navbar.tsx

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -69,7 +69,6 @@ export const Navbar = ({ children, className }: NavbarProps) => {
   return (
     <motion.div
       ref={ref}
-      // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
       className={cn("fixed inset-x-0 top-10 z-40 w-full", className)}
     >
       {React.Children.map(children, (child) =>
